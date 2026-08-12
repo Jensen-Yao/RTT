4cac540918fd44df2f9a0ecffe19c4605fa76730
Jensen-Yao <203140439+Jensen-Yao@users.noreply.github.com>
2026-08-12 20:28:13 +0800

    fix(web): publish resilient local translator

--- _astro/__vite-browser-external.BSsyfsem.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./translation.wwI8tAL7.js";var t=e(((e,t)=>{t.exports={}}));export default t();
---
+++ _astro/__vite-browser-external.COMXj6iN.js
@@ -0,0 +1,1 @@
+import{b as e}from"./translation.CGluRyC0.js";var t=e(((e,t)=>{t.exports={}}));export default t();
